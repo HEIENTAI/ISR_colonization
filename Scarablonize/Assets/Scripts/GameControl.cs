9189df6628ce0d4cc6cfc1d5a3ef2e2e48b59df1
Ying-Yi Huang <silverwings999@gmail.com>
2013-09-07 15:20:38 +0800

    上傳了一些程式碼接口

---
+++ Scarablonize/Assets/Scripts/GameControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//遊戲進行狀態
+public enum PlayStatus
+{
+    GameTitle,
+    GameChooseChapter,
+    GameChoosePlaymode,
+    RoundHumanTurn,
+    HumanTurnAnimating, // 人類方動畫撥放中, 無法操作
+    RoundScarabTurn,
+    ScarabTurnAnimating // 蟲族方動畫撥放中, 無法操作
+}
+
+//遊戲邏輯主控台
+public class GameControl{
+    public static GameControl _instance;
+    private PlayStatus _currentPlayStatus;
+
+    public static GameControl Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new GameControl();
+
+            return _instance;
+        }
+    }
+
+    public PlayStatus GameStatus
+    {
+        get
+        {
+            return _currentPlayStatus;
+        }
+    }
+
+    // 玩家從 title 畫面, 觸發遊戲開始
+    public void TriggerGameEnter()
+    {
+        if(_currentPlayStatus != PlayStatus.GameTitle)
+        {
+            DebugLog("Status error.");
+            return;
+        }
+
+        _currentPlayStatus = PlayStatus.GameChooseChapter;
+
+        // NGUI show ui
+    }
+
+    // UI 控制區 (NGUI)
+
+    // Scene (tile, Orthella)
+
+    public void DebugLog(string msg)
+    {
+        Debug.Log(msg);
+    }
+}
--- Scarablonize/Assets/Scripts/PlayStatus.cs
+++ Scarablonize/Assets/Scripts/PlayStatus.cs
@@ -1,21 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-//遊戲進行狀態
-public enum PlayStatus
-{
-    GameTitle, 
-    GameChooseChapter,
-    GameChoosePlaymode,
-    RoundHumanTurn,
-    RoundScarabTurn
-}
 
-
-// 玩家遊戲狀態, 主控台
+// 玩家遊戲狀態, 主控台 , 可能要砍, wait
 public class PlayStatusControl
 {
-
 	// Use this for initialization
 	void Start () {
 	
