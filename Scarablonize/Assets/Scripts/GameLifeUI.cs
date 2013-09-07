3d8d80c49af4c47447ddaa5ff3a795d293d7ab85
sadwx <sadwx4017@gmail.com>
2013-09-07 15:07:14 +0800

    add Life Game UI script

--- Scarablonize/Assets/Scripts/GameLifeUI.cs
+++ Scarablonize/Assets/Scripts/GameLifeUI.cs
@@ -3,16 +3,25 @@ using System.Collections;
 
 public class GameLifeUI : MonoBehaviour 
 {
-	UISprite _lifeBG;
-//	float 
+	public UISprite LifeBG;
+	public UILabel LifeDisplay;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+	
+	public int Count
+	{
+		set
+		{
+			LifeBG.fillAmount = (float)value / (float)(Const.MapSize * Const.MapSize);
+			LifeDisplay.text = value.ToString();
+		}
 	}
 }
