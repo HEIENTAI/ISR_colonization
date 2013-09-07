f6fc57f9824357bbcdbd9b3e13abafd4e92ec466
Ying-Yi Huang <silverwings999@gmail.com>
2013-09-07 14:47:04 +0800

    加了一個 script to unicode

---
+++ Scarablonize/Assets/Scripts/PlayStatus.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//遊戲進行狀態
+public enum PlayStatus
+{
+    GameTitle, 
+    GameChooseChapter,
+    GameChoosePlaymode,
+    RoundHumanTurn,
+    RoundScarabTurn
+}
+
+
+// 玩家遊戲狀態, 主控台
+public class PlayStatusControl
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+	}
+}
