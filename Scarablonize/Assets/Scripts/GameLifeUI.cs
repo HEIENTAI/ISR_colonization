018c05bcd158dae6cb342c67ff802a838a35e342
sadwx <sadwx4017@gmail.com>
2013-09-07 16:51:11 +0800

    game life count display move along with bar length.

--- Scarablonize/Assets/Scripts/GameLifeUI.cs
+++ Scarablonize/Assets/Scripts/GameLifeUI.cs
@@ -5,6 +5,21 @@ public class GameLifeUI : MonoBehaviour
 {
 	public UISprite LifeBG;
 	public UILabel LifeDisplay;
+	
+	float _barLength;
+	float _lifeDisplaytopPos;
+	
+	void Initialize()
+	{
+		Count = 0;
+	}
+	
+	void Awake()
+	{
+		_barLength = LifeBG.cachedTransform.localScale.y;
+		_lifeDisplaytopPos = LifeDisplay.cachedTransform.localPosition.y;
+		Initialize();
+	}
 
 	// Use this for initialization
 	void Start () 
@@ -22,6 +37,7 @@ public class GameLifeUI : MonoBehaviour
 		{
 			LifeBG.fillAmount = (float)value / (float)(Const.MapSize * Const.MapSize);
 			LifeDisplay.text = value.ToString();
+			LifeDisplay.cachedTransform.localPosition = new Vector3(0, _lifeDisplaytopPos - (_barLength * (1 - LifeBG.fillAmount)), 0);
 		}
 	}
 }
