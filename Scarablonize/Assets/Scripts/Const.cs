06769ad37b96efbf1bc250f87deb479284409010
sadwx <sadwx4017@gmail.com>
2013-09-08 15:38:47 +0800

    - add game result UI. - add button tap sound.

--- Scarablonize/Assets/Scripts/Const.cs
+++ Scarablonize/Assets/Scripts/Const.cs
@@ -38,42 +38,4 @@ public enum PlayMode
 {
     SinglePlay,
     TwoPlayer
-}
-public enum ShowStatus{	Hide,	BringIn,	BringOut}
-public enum Creature
-{
-    None = 0, // 無
-    People = 1, // 人
-    Scarab = 2, // 甲蟲
-}
-
-public enum BlockType
-{
-    River = 0, // 河流
-    Sand = 1, // 沙漠
-    Pyramid = 2, // 金字塔
-    Hole = 3, // 蟲洞
-    House = 4, // 房子
-}
-
-public enum MoveType
-{
-    None,   // 無
-    Move,   // 移動    
-    Clone,  // 需要複製的移動
-}
-
-public enum BattleResult
-{
-    PeopleWin, // 人類贏了
-    ScarabWin, // 甲蟲贏了
-    Draw, // 平手
-}
-
-public enum ControlMessage
-{
-    OK, // 可以移動
-    WrongCreature, // 控制的生物種類錯誤
-    NoneCreature, // 要控制的位置沒有生物
-    PositionError, // 位置錯誤
-}public class TagConst{	public const string SinglePlay = "1P";	public const string TwoPlayers = "2P";	public const string Staff = "Staff";	public static readonly string[] Levels = new string[]	{		"Level1",		"Level2",		"Level3",		"Level4"	};}
+}public enum ShowStatus{	Hide,	BringIn,	BringOut}public enum Creature{    None = 0, // 無    People = 1, // 人    Scarab = 2, // 甲蟲}public enum BlockType{    River = 0, // 河流    Sand = 1, // 沙漠    Pyramid = 2, // 金字塔    Hole = 3, // 蟲洞    House = 4, // 房子}public enum MoveType{    None,   // 無    Move,   // 移動        Clone,  // 需要複製的移動}public enum BattleResult{    PeopleWin, // 人類贏了    ScarabWin, // 甲蟲贏了    Draw, // 平手}public enum ControlMessage{    OK, // 可以移動    WrongCreature, // 控制的生物種類錯誤    NoneCreature, // 要控制的位置沒有生物    PositionError, // 位置錯誤}public class TagConst{	public const string SinglePlay = "1P";	public const string TwoPlayers = "2P";	public const string Staff = "Staff";	public static readonly string[] Levels = new string[]	{		"Level1",		"Level2",		"Level3",		"Level4"	};	public const string Scarab = "Scarab";	public const string Human = "Human";}
